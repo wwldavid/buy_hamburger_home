a4d1926b443cc1dfb3f9bcd504ad3663ad392e6a
davidwanlongwu <davidwu.web@gmail.com>
2025-03-23 12:10:33 -0600

    checkout header and footer

--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Checkout.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import CartContext from "../../../store/cart-context";
 
 const checkoutRoot = document.getElementById("checkout-root");
 
 const Checkout = (props) => {
+  const ctx = useContext(CartContext);
+
   return ReactDOM.createPortal(
     <div className={classes.Checkout}>
       <div className={classes.Close}>
         <FontAwesomeIcon onClick={() => props.onHide()} icon={faXmark} />
+      </div>
+
+      <div className={classes.MealsDesc}>
+        <header className={classes.Header}>
+          <h2 className={classes.Title}>meal list</h2>
+        </header>
+        <div>meal list</div>
+        <footer className={classes.Footer}>
+          <p className={classes.TotalPrice}>{ctx.totalPrice}</p>
+        </footer>
       </div>
     </div>,
     checkoutRoot
